467f1c5a450c997f0073bc7f511e2d6e1ad25640
David Osborne <156497440+DavidJOsborne@users.noreply.github.com>
2025-04-14 08:51:55 -0500

    Create query.sql

---
+++ basketball/query.sql
@@ -0,0 +1,5 @@
+SELECT * FROM wolvesStats;
+
+SELECT id, name, gp FROM wolvesStats WHERE id = 1;
+SELECT id, name, gp FROM wolvesStats WHERE id = 3;
+SELECT id, name, gp FROM wolvesStats WHERE id = 7;
